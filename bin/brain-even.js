262cc1d458dbaee786efa896942224f3979f4b7c
Magomed Ilyasov <mkznworkflow@gmail.com>
2020-11-19 00:01:08 +0300

    new games

---
+++ bin/brain-even.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import name from '../src/cli.js';
+
+console.log(`Hello, ${name}!`);
+
+const randomNumber = 0; 
+if (name !== 0) {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+}
