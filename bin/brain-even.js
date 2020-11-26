02580ef72f976cc97969220ab51a7c766ecf2794
Magomed Ilyasov <mkznworkflow@gmail.com>
2020-11-27 00:18:56 +0300

    Added random numbers

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,8 +2,18 @@
 import name from '../src/cli.js';
 
 console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
-const randomNumber = 0; 
-if (name !== 0) {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+// const randomNumber = Math.floor(Math.random() * 100);
+// console.log(`Question: ${randomNumber}`);
+
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Max and min includes
+}
+console.log(`Question: ${getRandomIntInclusive(0,100)}`);
+
+if (getRandomIntInclusive % 2 === 0) {
+
 }
