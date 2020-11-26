fa5b977911c0c41a69805bafb527e3f7cc4773f4
Magomed Ilyasov <mkznworkflow@gmail.com>
2020-11-27 00:37:04 +0300

    Added answers

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 import name from '../src/cli.js';
+import readlineSync from 'readline-sync';
 
 console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
-// const randomNumber = Math.floor(Math.random() * 100);
-// console.log(`Question: ${randomNumber}`);
 
 const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Max and min includes
 }
+
 console.log(`Question: ${getRandomIntInclusive(0,100)}`);
 
-if (getRandomIntInclusive % 2 === 0) {
+const giveAnswer = readlineSync.question('Your answer: ');
+
+if (getRandomIntInclusive % 2 === 0 && giveAnswer === 'yes') {
 
 }
