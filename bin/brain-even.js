7cd4a4dbbe95e8457aa1d04e98558e1882395357
Magomed Ilyasov <mkznworkflow@gmail.com>
2020-11-29 18:45:37 +0300

    added new strings

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -11,11 +11,18 @@ const getRandomIntInclusive = (min, max) => {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Max and min includes
 }
-
+let randomNumber = getRandomIntInclusive;
 console.log(`Question: ${getRandomIntInclusive(0,100)}`);
 
 const giveAnswer = readlineSync.question('Your answer: ');
+let result = 0;
+let correctAnswersCheck = 3; 
+// const wrongAnswer = console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
 
-if (getRandomIntInclusive % 2 === 0 && giveAnswer === 'yes') {
-
+for (let i = 0; i < correctAnswersCheck; i += 1) {
+  if (randomNumber % 2 === 0 && giveAnswer === 'yes') {
+    result += 1;
+  } if (randomNumber % 2 === 1 && giveAnswer === 'no') {
+    result += 1;
+  }
 }
