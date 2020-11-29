dd18d310f1445b0e2a092f6953b799ddaf168c5b
Magomed Ilyasov <mkznworkflow@gmail.com>
2020-11-29 22:03:57 +0300

    Complited brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -11,19 +11,26 @@ const getRandomIntInclusive = (min, max) => {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Max and min includes
 }
-let randomNumber = getRandomIntInclusive;
-console.log(`Question: ${getRandomIntInclusive(0,100)}`);
 
-const giveAnswer = readlineSync.question('Your answer: ');
-let checkAnswer = giveAnswer
 let result = 0;
-let correctAnswersCheck = 3; 
-// const wrongAnswer = console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+let correctAnswersCheck = 3;
 
 for (let i = 0; i < correctAnswersCheck; i += 1) {
-  if (randomNumber % 2 === 0 && checkAnswer === 'yes') {
-    
-  } if (randomNumber % 2 === 1 && checkAnswer === 'no') {
-    
+  let randomNumber = getRandomIntInclusive(0, 100);
+  console.log(`Question: ${randomNumber}`);
+  const giveAnswer = readlineSync.question('Your answer: ');
+  if (randomNumber % 2 === 0 && giveAnswer === 'yes') {
+    result += 1;
+    console.log('Correct!');
+  } else if (randomNumber % 2 === 1 && giveAnswer === 'no') {
+    result += 1;
+    console.log('Correct!');
+  } else {
+    break;
   }
+} 
+if (result === correctAnswersCheck) {
+  console.log(`Congratulations, ${name}!`)
+} else {
+  console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
 }
