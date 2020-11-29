ffbc23e76d9bf770a1cf87aa9c52a9d264b83009
Magomed Ilyasov <mkznworkflow@gmail.com>
2020-11-29 22:21:56 +0300

    fixed errors

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
-import name from '../src/cli.js';
 import readlineSync from 'readline-sync';
+import name from '../src/cli.js';
 
 console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".')
-
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const getRandomIntInclusive = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Max and min includes
-}
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue; // Max and min includes
+};
 
 let result = 0;
-let correctAnswersCheck = 3;
+const correctAnswersCheck = 3;
 
 for (let i = 0; i < correctAnswersCheck; i += 1) {
-  let randomNumber = getRandomIntInclusive(0, 100);
+  const randomNumber = getRandomIntInclusive(0, 100);
   console.log(`Question: ${randomNumber}`);
   const giveAnswer = readlineSync.question('Your answer: ');
   if (randomNumber % 2 === 0 && giveAnswer === 'yes') {
@@ -28,9 +27,10 @@ for (let i = 0; i < correctAnswersCheck; i += 1) {
   } else {
     break;
   }
-} 
+}
+
 if (result === correctAnswersCheck) {
-  console.log(`Congratulations, ${name}!`)
+  console.log(`Congratulations, ${name}!`);
 } else {
   console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
 }
