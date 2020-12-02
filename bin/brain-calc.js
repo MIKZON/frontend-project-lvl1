8cb633016fc70ce8c083953855902ecbaf8944ab
Magomed Ilyasov <mkznworkflow@gmail.com>
2020-12-02 22:34:51 +0300

    brain-calc game added

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -7,23 +7,25 @@ console.log(`Hello, ${name}!`);
 console.log('What is the result of the expression?');
 
 const operations = ['+', '-', '*'];
-const randomArr = Math.floor(Math.random() * operations.length);
-console.log(operations[randomArr]);
-
 const correctAnswersCheck = 3;
+let result = 0;
 
 for (let i = 0; i < correctAnswersCheck; i += 1) {
   const randomNumberOne = getRandomIntInclusive(20, 100);
   const randomNumberTwo = getRandomIntInclusive(0, 30);
+  const randomArr = Math.floor(Math.random() * operations.length);
+  let calculationsNumbers = eval(`${randomNumberOne} ${operations[randomArr]} ${randomNumberTwo}`).toString();
   console.log(`Question: ${randomNumberOne} ${operations[randomArr]} ${randomNumberTwo}`);
   const giveAnswer = readlineSync.question('Your answer: ');
-  if (randomNumber % 2 === 0 && giveAnswer === 'yes') {
-    result += 1;
-    console.log('Correct!');
-  } else if (randomNumber % 2 === 1 && giveAnswer === 'no') {
+  if (calculationsNumbers === giveAnswer) {
     result += 1;
     console.log('Correct!');
   } else {
+    console.log(`'${giveAnswer}' is wrong answer ;(. Correct answer was '${calculationsNumbers}'. Let's try again, ${name}!`);
     break;
   }
+}
+
+if (result === correctAnswersCheck) {
+  console.log(`Congratulations, ${name}!`);
 }
