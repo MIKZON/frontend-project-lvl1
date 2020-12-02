f7b83e88ffadbd289f2520cbca5ec4f0eee09ddc
Magomed Ilyasov <mkznworkflow@gmail.com>
2020-12-02 15:05:41 +0300

    added operators in game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -7,13 +7,15 @@ console.log(`Hello, ${name}!`);
 console.log('What is the result of the expression?');
 
 const operations = ['+', '-', '*'];
+const randomArr = Math.floor(Math.random() * operations.length);
+console.log(operations[randomArr]);
 
 const correctAnswersCheck = 3;
 
 for (let i = 0; i < correctAnswersCheck; i += 1) {
   const randomNumberOne = getRandomIntInclusive(20, 100);
   const randomNumberTwo = getRandomIntInclusive(0, 30);
-  console.log(`Question: ${randomNumberOne} ${randomNumberTwo}`);
+  console.log(`Question: ${randomNumberOne} ${operations[randomArr]} ${randomNumberTwo}`);
   const giveAnswer = readlineSync.question('Your answer: ');
   if (randomNumber % 2 === 0 && giveAnswer === 'yes') {
     result += 1;
